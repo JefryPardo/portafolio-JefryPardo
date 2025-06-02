574bee5bca6dec1319305fee9018ed58e56d3f26
Jefry Pardo <jeffry.pardo01@unicatolica.edu.co>
2025-06-02 01:17:03 -0500

    se aplica algunos estilos y cuadros svg

--- src/app/module/portafolio-2025/components/about/about.component.ts
+++ src/app/module/portafolio-2025/components/about/about.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  cuadros: number[] = [];
+
+  tipos = ['space', 'white', 'black'];
+
+  maxCuadros = 55;
+
+  constructor() {
+    this.generarCuadros();
+  }
+
+  generarCuadros() {
+    this.cuadros = Array.from({ length: this.maxCuadros }, () => {
+      const index = Math.floor(Math.random() * this.tipos.length);
+      return index;
+    });
+  }
 }
